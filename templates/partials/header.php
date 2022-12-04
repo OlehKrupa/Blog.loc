e09e65d8ac1dff8679e33014f672ab5a199ac660
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-04 17:33:32 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -16,29 +16,24 @@
     <div class="container text-center">
         <nav class="navbar navbar-expand-lg bg-light mb-2">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Навбар</a>
+                <a class="navbar-brand" href="#">БлогерЮА</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="404.html">Домой</a>
+                            <a class="nav-link active" aria-current="page" href="404.html">Додому</a>
                         </li>
-
-                        <li class="nav-item">
-                            <select name="language" class="form-select" id="language">
-                                <?php
-                                foreach($mas as $key=>$v)
-                                    echo "<option value='$v'>$v</option>";
-                                ?>
-                            </select>
-                        </li>
-
                     </ul>
+
+                    <form class="d-flex mx-2">
+                        <button type="button" class="btn btn-primary">Увійти</button>
+                    </form>
+
                     <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <input class="form-control me-2" type="search" placeholder="Пошук статті" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Пошук</button>
                     </form>
                 </div>
             </div>
