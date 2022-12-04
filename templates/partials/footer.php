42a9dd9ec81cb3ffa4b9d410e9e45891ec4cbdba
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-04 17:33:11 +0200

    Make simple footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,2 +1,22 @@
+<footer class="text-center pt-4 my-md-5 pt-md-5 border-top">
+<div class="row">
+ 		<div class="col-6 col-md">
+ 			<h5>Footer</h5>
+ 			<ul class="list-unstyled text_small">
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 			</ul>
+ 		</div> 
+ 		<div class="col-6 col-md">
+ 			<h5>Footer</h5>
+ 			<ul class="list-unstyled text_small">
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 				<li><a href="" class="link-secondary">Footer</a></li>
+ 			</ul>
+ 		</div> 
+</div>
+</footer>
 </body>
 </html>
