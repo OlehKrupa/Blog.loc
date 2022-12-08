2042481e776744b7cd2db67a5cee3515083f03f7
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-08 18:38:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
+session_start();
+
+define("USER_NAME","Zhabka");
+define("USER_PASSWORD","1111");
+
 define("ROOT_PATH", dirname(__FILE__));
 define("TEMPLATES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
-session_start();
+define("DB_NAME","blog");
+define("DB_USER","db_user");
+define("DB_USER_PASS","1111");
+
+
+$db_connect = new PDO('mysql:host=localhost;dbname'.DB_NAME,DB_USER,DB_USER_PASS);
+
+?>
