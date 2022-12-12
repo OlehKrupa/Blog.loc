08488df4ac0b707059788e7a34e3f22a2711d7d1
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-12 19:32:41 +0200

    Validation
    Validation

--- login.php
+++ login.php
@@ -1,19 +1,33 @@
 <?php
 require_once "config.php";
 
-if (!empty($_POST['login_name']) && !empty($_POST['login_password'])){
-	if ($_POST['login_name'] === USER_NAME && $_POST['login_password'] === USER_PASSWORD){
-		$_SESSION['user']=$_POST['login_name'];
-		header("location: /index.php");
-		die();
+$fields = ["login_name","login_password"];
+
+if (!empty($_POST)){
+	$error = [];
+	foreach ($_POST as $k => $v) {
+		if (in_array($k, $fields) && empty($v)){
+			$error[$k]="field must be filled!";
+		}
 	}
-	else{
+
+	if (empty($error))
+	{
+		if ($_POST['login_name'] === USER_NAME && $_POST['login_password'] === USER_PASSWORD){
+			$_SESSION['user']=$_POST['login_name'];
+			header("location: /index.php");
+			die();
+		}
+		else{
 		//Типу якщо нічого не ввели то входимо як гість
-		$_SESSION['user']='guest';
-		header("location: /index.php");
-		die();
+		//Але так не працює
+			$_SESSION['user']='guest';
+			header("location: /index.php");
+			die();
+		}
 	}
 }
+
 
 require_once TEMPLATES_PATH."login.html";
 ?>
