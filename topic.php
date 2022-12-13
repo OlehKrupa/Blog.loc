dfbca89fde3a519e96e6f06022308ba2ebb7585c
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-13 17:30:31 +0200

    Create topic.php

---
+++ topic.php
@@ -0,0 +1,17 @@
+<?php
+require_once "config.php";
+
+if (empty($_POST["select_category"])){
+	$select=0;
+} else{
+	$select = $_POST["select_category"];
+}
+
+$stmt=$dbConnect->prepare(
+	"SELECT `text` from `articles` where `moderated` = '1' and `category_id`= :selected_category+1");
+
+$stmt->execute(["selected_category"=>$select]);
+$topics = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+require_once TEMPLATES_PATH."index.html";
+?>
