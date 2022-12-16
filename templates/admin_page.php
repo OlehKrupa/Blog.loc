5500c0fb186a46d119bec89992053797b6124797
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-16 23:11:33 +0200

    Update admin_page.php

--- templates/admin_page.php
+++ templates/admin_page.php
@@ -5,28 +5,29 @@
     </div>
 
     <div class="col-8">
-       <h3>Модерация</h3>
+     <h3>Модерация</h3>
+     <form action="admin.php" method="post"> 
 
-       <div class="row"> 
-        <div class="input-group">
-            <label class="input-group-text" for="select_category">Тематика</label>
-            <select name="select_category" class="form-select" id="select_category">
-                <?php require_once "category.php";
+         <div class="row"> 
+            <div class="input-group">
+                <label class="input-group-text" for="select_category">Тематика</label>
+                <select name="select_category" class="form-select" id="select_category">
+                    <?php require_once "category.php";
                     foreach ($categories as $key => $value):?>
-                    <option  value='<?php echo $key;?>' <?php if($key===$category-1) echo "selected"; ?> > <?php echo $value;?></option>
+                        <option  value='<?php echo $key;?>' <?php if($key===$category-1) echo "selected"; ?> > <?php echo $value;?></option>
                     <?php endforeach; ?>
                 </select>
 
             </div>
 
             <div class="input-group mt-2">
-                <span class="input-group-text" id="basic-addon1">Заголовок: </span>
+                <span class="input-group-text" id="basic-addon1">Заголовок</span>
                 <input type="text" class="form-control" aria-describedby="basic-addon1"  id="title" name="title" value="<?php echo $title; ?>">
             </div>
 
             <div class="col"> 
                 <div class="input-group mt-2">
-                    <span class="input-group-text" id="basic-addon1" >Никнейм: </span>
+                    <span class="input-group-text" id="basic-addon1" >Нікнейм</span>
                     <input type="text" class="form-control" aria-describedby="basic-addon1"  id="name" name="name" readonly="readonly" value="<?php echo $name; ?>">
                 </div>
             </div>
@@ -46,16 +47,16 @@
         </div>
 
         <div class="col">
-            <form action="admin.php" method="post"> 
-                <button class="btn btn-success" type="submit" id="approve" name="approve">Одобрить</button>
-                <button class="btn btn-warning" type="submit" id="next" name="next">Следующее</button>
-                <button class="btn btn-danger" type="submit" id="delete" name="delete">Удалить</button>
-            </form>
+            <button class="btn btn-success" type="submit" id="approve" name="approve">База</button>
+            <button class="btn btn-warning" type="submit" id="next" name="next">Наступний</button>
+            <button class="btn btn-warning" type="submit" id="prev" name="prev">Попередній</button>
+            <button class="btn btn-danger" type="submit" id="delete" name="delete">Антибаза</button>
         </div>
-    </div>
+    </form>
+</div>
 
-    <div class="col-2">
-        <?php echo("Адмін: ".$_SESSION['user']) ?>
-    </div>
+<div class="col-2">
+
+</div>
 </div>
 <?php include TEMPLATES_PATH."partials".DIRECTORY_SEPARATOR."footer.php";?>
