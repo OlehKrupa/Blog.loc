b7ed216fd9c2df796dfb68e5550216fa857400d1
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-16 21:58:56 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,12 +1,31 @@
 <?php
 require_once "config.php";
 
-$result = $dbConnect->query("SELECT `a`.`category_id`, `u`.`nickname`,`a`.`title`,`a`.`text`,`a`.`date` FROM `articles` as `a`
+$result = $dbConnect->query("SELECT `a`.`id`, `a`.`category_id`, `u`.`nickname`,`a`.`title`,`a`.`text`,`a`.`date` FROM `articles` as `a`
 INNER join `user` as `u` on `a`.`user_id`=`u`.`id`
 where `moderated`=0");
 $articles = $result->fetchAll(PDO::FETCH_ASSOC);
 
-$current_article=1;
+$article_id=[];
+
+foreach ($articles as $value) {
+	array_push($article_id, $value['id']);
+}
+
+if (empty($_SESSION['current_article'])){
+	$_SESSION['current_article']=0;
+} 
+
+if (isset($_POST['next'])){
+	$_SESSION['current_article']++;
+
+	if ($_SESSION['current_article']> count($article_id)-1)
+	{
+		$_SESSION['current_article']=0;
+	}
+}
+
+$current_article=$_SESSION['current_article'];
 
 $category=$articles[$current_article]['category_id'];
 $title = $articles[$current_article]['title'];
@@ -14,9 +33,9 @@ $name = $articles[$current_article]['nickname'];
 $date = $articles[$current_article]['date'];
 $text= $articles[$current_article]['text'];
 
-echo "<pre>";
-print_r($articles);
-echo "</pre>";
+//echo "<pre>";
+//print_r($_SESSION['current_article']);
+//echo "</pre>";
 
-require_once TEMPLATES_PATH."admin.html";
+require_once TEMPLATES_PATH."admin_page.php";
 ?>
