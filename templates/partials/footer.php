e0eb81bdc44184c46caefaf8d87cde7a75ccea2d
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-19 17:20:34 +0200

    Update footer.php

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,21 +1,13 @@
 <footer class="text-center pt-4 my-md-5 pt-md-2 border-top">
 	<div class="row">
-		<div class="col-6 col-md">
-			<h5>Footer</h5>
+		<div class="col col-md">
+			<h5>Індивідуальне завдання</h5>
 			<ul class="list-unstyled text_small">
-				<li><a href="" class="link-secondary">Footer</a></li>
-				<li><a href="" class="link-secondary">Footer</a></li>
-				<li><a href="" class="link-secondary">Footer</a></li>
+				<li><a href="" class="link-secondary">Умова завдання</a></li>
+				<li><a href="" class="link-secondary">Розробник</a></li>
 			</ul>
 		</div> 
-		<div class="col-6 col-md">
-			<h5>Footer</h5>
-			<ul class="list-unstyled text_small">
-				<li><a href="" class="link-secondary">Footer</a></li>
-				<li><a href="" class="link-secondary">Footer</a></li>
-				<li><a href="" class="link-secondary">Footer</a></li>
-			</ul>
-		</div> 
+
 	</div>
 </footer>
 </body>
