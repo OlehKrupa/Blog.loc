7fae5d88a949d7211152fc56b322c2b3ded841d6
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-19 17:20:37 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -13,22 +13,22 @@
     <div class="container text-center">
         <nav class="navbar navbar-expand-lg bg-light mb-2">
             <div class="container-fluid">
-                <a class="navbar-brand" id="blog" name="blog" type="submit">БлогерЮА</a>
+                <a class="navbar-brand" id="blog" name="blog" type="submit" href="blog.php">БлогерЮА</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Додому</a>
+                            <a class="nav-link active" aria-current="page" href="index.php"><?php if ($_SESSION['user_role']==='admin') echo "Адміністрування"; else echo "Додому";?></a>
                         </li>
 
                         <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="login.php">Увійти</a>
+                            <a class="nav-link active" aria-current="page" href="login.php">Увійти</a>
                         </li>
 
                         <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="new_topic.php">Написати</a>
+                            <a class="nav-link active" aria-current="page" href="new_topic.php">Написати</a>
                         </li>
                     </ul>
 
